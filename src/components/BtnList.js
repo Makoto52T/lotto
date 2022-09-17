6fddb95ef2e7b568032d83ab3e08845525703e78
Makoto52T <toonteamm@gmail.com>
2022-09-18 00:30:47 +0700

    delete hook

--- src/components/BtnList.js
+++ src/components/BtnList.js
@@ -1,12 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import {Button} from 'react-bootstrap';
 
 export default function BtnList({value, index, config, variant}) {
-  useEffect (() => {
-    if (config) {
-      console.log (config);
-    }
-  }, []);
   return (
     <Button
       key={index}
